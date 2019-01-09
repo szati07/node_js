608d81478f36b60b752ef9070c11e2d5486cd1e4
Bence Szatmári <szatmari.bence@lexunit.hu>
2019-01-09 16:31:23 +0100

    added some mysql logic and connection to database

--- 00-starting-setup/controllers/shop.js
+++ 00-starting-setup/controllers/shop.js
@@ -2,34 +2,39 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-	Product.fetchAll(products => {
-		res.render('shop/product-list', {
-			prods: products,
-			pageTitle: 'All Products',
-			path: '/products'
-		});
-	});
+	Product.fetchAll()
+		.then(([rows]) => {
+			res.render('shop/product-list', {
+				prods: rows,
+				pageTitle: 'All Products',
+				path: '/products'
+			});
+		})
 };
 
 exports.getProduct = (req, res, next) => {
 	const prodId = req.params.productId;
-	Product.findById(prodId, product => {
-		res.render('shop/product-detail', {
-			pageTitle: product.title,
-			path: '/',
-			product: product
+	Product.findById(prodId)
+		.then(([product]) => {
+			res.render('shop/product-detail', {
+				pageTitle: product[0].title,
+				path: '/',
+				product: product[0]
+			})
 		})
-	});
+		.catch(err => console.log(err));
 }
 
 exports.getIndex = (req, res, next) => {
-	Product.fetchAll(products => {
-		res.render('shop/index', {
-			prods: products,
-			pageTitle: 'Shop',
-			path: '/'
-		});
-	});
+	Product.fetchAll()
+		.then(([rows, fieldData]) => {
+			res.render('shop/index', {
+				prods: rows,
+				pageTitle: 'Shop',
+				path: '/'
+			});
+		})
+		.catch(err => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
@@ -39,7 +44,7 @@ exports.getCart = (req, res, next) => {
 			for (product of products) {
 				const cartProductData = cart.products.find(prod => prod.id === product.id);
 				if (cartProductData) {
-					cartProducts.push({productData: product, qty: cartProductData.qty})
+					cartProducts.push({ productData: product, qty: cartProductData.qty })
 				}
 			}
 			res.render('shop/cart', {
