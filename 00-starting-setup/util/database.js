561aac4c6e0ff5ff8760f4bd96ed98d245aa34ba
Bence Szatmári <szatmari.bence@lexunit.hu>
2019-01-09 15:54:46 +0100

    added routing, database to node project

---
+++ 00-starting-setup/util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Szati.095'
+});
+
+module.exports = pool.promise();
